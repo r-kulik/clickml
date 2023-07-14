48609fb16e057a30b47299de09c18a817f8c8453
Ernest <58702017+ErnestMatskevich@users.noreply.github.com>
2023-07-14 22:30:44 +0300

    Error message, Ml Model etc.

---
+++ server/static/js/model_settings.js
@@ -0,0 +1,24 @@
+       var is_checked_one = false;
+        var old_label;
+        function showValue(id) {
+            var radios = document.getElementsByName('target_variable');
+            for (var i = 0; i < radios.length; i++) {
+
+                if (radios[i].checked) {
+                    document.getElementById('text').innerHTML = radios[i].value;
+                    var label = document.getElementById(id+"1");
+                    label.style.background = "#79E34F";
+                    if(how){
+                        old_label.style.background = 'initial';
+                        how = false;
+                    }
+                    old_label = label;
+                    is_checked_one = true;
+
+                    break;
+
+                }
+            }
+
+
+        }
