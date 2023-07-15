3ff16016edbdb571db59aa181cf75d9ddd8598dd
Rostislav Kulik <45662763+r-kulik@users.noreply.github.com>
2023-07-15 23:03:51 +0300

    And now we ve solved problems

--- server/static/js/model_settings.js
+++ server/static/js/model_settings.js
@@ -1,24 +1,24 @@
-       var is_checked_one = false;
-        var old_label;
-        function showValue(id) {
-            var radios = document.getElementsByName('target_variable');
-            for (var i = 0; i < radios.length; i++) {
+var is_checked_one = false;
+var old_label;
+function showValue(id) {
+    var radios = document.getElementsByName('target_variable');
+    for (var i = 0; i < radios.length; i++) {
 
-                if (radios[i].checked) {
-                    document.getElementById('text').innerHTML = radios[i].value;
-                    var label = document.getElementById(id+"1");
-                    label.style.background = "#79E34F";
-                    if(how){
-                        old_label.style.background = 'initial';
-                        how = false;
-                    }
-                    old_label = label;
-                    is_checked_one = true;
-
-                    break;
-
-                }
+        if (radios[i].checked) {
+            document.getElementById('text').innerHTML = radios[i].value;
+            var label = document.getElementById(id+"1");
+            label.style.background = "#79E34F";
+            if(is_checked_one){
+                old_label.style.background = 'initial';
+                is_checked_one = false;
             }
+            old_label = label;
+            is_checked_one = true;
 
+            break;
 
         }
+    }
+
+
+}
