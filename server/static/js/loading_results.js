23263083ecc222e61814e1ee45408bca1bd3cc66
Ernest <58702017+ErnestMatskevich@users.noreply.github.com>
2023-07-17 00:59:36 +0300

    Create frontend and addednice graph into main page

--- server/static/js/loading_results.js
+++ server/static/js/loading_results.js
@@ -5,12 +5,23 @@ async function createSocketAndConnect(exploit_task_id) {
 
     waitSocket.onmessage = function (message) {
         var text = message.data;
+
         const data = JSON.parse(text);
         if (data.finish === 1 && data.task_id === exploit_task_id){
+            var image = document.getElementById("loading");
+
+            image.setAttribute("style", "display: none;")
             document.getElementById(
                 "link_to_download").setAttribute(
                     "href",
                     "/download_results?task_id="+exploit_task_id);
+            document.getElementById(
+                "link_to_download").setAttribute(
+                    "style",
+                    "display: block;text-decoration: none;");
+
+
+
         }
     }
 
